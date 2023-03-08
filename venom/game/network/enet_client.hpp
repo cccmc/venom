6394a96b5d4e7943776c50ae35b98f5c6b6048db
evous <52077329+evous@users.noreply.github.com>
2023-03-08 14:35:36 +0300

    unknown variables

--- venom/game/network/enet_client.hpp
+++ venom/game/network/enet_client.hpp
@@ -9,17 +9,17 @@ struct enet_client_t {
 	void* host; // 0xa0
 	void* peer; // 0xa8
 	std::uint32_t timed_out_timestamp;
-	std::uint32_t unk0;
-	std::uint32_t unk1;
-	std::uint32_t unk2;
+	std::uint32_t tracker_tick;
+	std::uint32_t network_message_type;
+	std::uint32_t unknown_timer;
 	std::string server_ip; // 0xc0
 	std::uint32_t server_port; // 0xe0
-	std::uint32_t unk3;
+	std::uint32_t unk;
 	std::int32_t token; // 0xe8
 	std::int32_t user; // 0xec
 	std::string door_id; // 0xf0
 	std::int32_t lmode; // 0x110
-	std::uint32_t unk4;
+	std::uint32_t unk2;
 	std::string uuid_token; // 0x118
 };
 #pragma pack(pop)
